1a64db327c33be41ff98fbd626e4a78b98d33dd3
Kitanrum <krystel.theuvenin@hotmail.com>
2016-08-01 14:42:53 -0400

    Dice added

---
+++ Assets/Rolls.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rolls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        StartCoroutine(Rolled());
+	
+	}
+
+    IEnumerator Rolled() {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            transform.Translate(130.0f * Vector3.up * Time.deltaTime, Space.World);
+            transform.Rotate(75.0f * Vector3.left , Space.World);
+                       
+        }
+
+        yield return new WaitForSeconds(5);
+
+    }
+}
